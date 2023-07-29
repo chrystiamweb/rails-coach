9bb8b0018432b285572cce94718e364ca2a74d73
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2023-07-29 04:45:24 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root "courses#index"
-  resources :lessons
-  resources :courses
+
   resources :customers
+
+  resources :courses do
+    resources :lessons
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
